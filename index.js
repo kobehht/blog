c4283515911851de4deb207696eb457b108785d0
kobehht <hhtkobebyant@163.com>
2017-01-05 10:23:37 +0800

    add navgation views

--- index.js
+++ index.js
@@ -1,9 +1,20 @@
+var path = require('path');
 var express = require('express');
 var routes = require('./routes');
 var config = require('config-lite');
-var pkg = require('./package')
+var pkg = require('./package');
 
 var app = express();
+
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.locals.blog = {
+    title: pkg.name,
+    description: pkg.description
+};
 
 routes(app);
 
